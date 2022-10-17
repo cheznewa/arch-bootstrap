d61afbfccb4528a50c228a2c714242b2f6e99375
cheznewa <chezsick@orange.fr>
2022-10-17 16:18:06 +0200

    add steam deck

---
+++ arch-bootstrap-steamdeck.sh
@@ -0,0 +1,271 @@
+#!/bin/bash
+#
+# arch-bootstrap: Bootstrap a base Arch Linux system using any GNU distribution.
+#
+# Dependencies: bash >= 4, coreutils, wget, sed, gawk, tar, gzip, chroot, xz, zstd.
+# Project: https://github.com/tokland/arch-bootstrap
+#
+# Install:
+#
+#   # install -m 755 arch-bootstrap-steamdeck.sh /usr/local/bin/arch-bootstrap
+#
+# Usage:
+#
+#   # arch-bootstrap-steamdeck destination
+#
+# And then you can chroot to the destination directory (user: user, password: root, and no password in sudo):
+#
+#   # chroot destination
+
+set -e -u -o pipefail
+
+# Packages needed by pacman (see get-pacman-dependencies.sh)
+PACMAN_PACKAGES=(
+  acl archlinux-keyring attr brotli bzip2 curl expat glibc gpgme libarchive
+  libassuan libgpg-error libnghttp2 libssh2 lzo openssl pacman xz zlib filesystem
+  krb5 e2fsprogs keyutils libidn2 libunistring gcc-libs lz4 libpsl icu libunistring zstd sudo
+  ca-certificates-utils ca-certificates-mozilla bash readline ncurses coreutils findutils # certificate probleme
+  p11-kit libp11-kit libtasn1 libffi # p11-kit and dependances
+  )
+BASIC_PACKAGES=(${PACMAN_PACKAGES[*]} filesystem)
+EXTRA_PACKAGES=(coreutils bash grep gawk file tar systemd sed)
+DEFAULT_REPO_URL="https://steamdeck-packages.steamos.cloud/archlinux-mirror/"
+DEFAULT_ARM_REPO_URL="http://eu.mirror.archlinuxarm.org"
+DEFAULT_POWER_REPO_URL="http://repo.archlinuxpower.org/base/"
+
+stderr() { 
+  echo "$@" >&2 
+}
+
+debug() {
+  stderr "--- $@"
+}
+
+extract_href() {
+  sed -n '/<a / s/^.*<a [^>]*href="\([^\"]*\)".*$/\1/p'
+}
+
+fetch() {
+  curl -L -s "$@"
+}
+
+fetch_file() {
+  local FILEPATH=$1
+  shift
+  if [[ -e "$FILEPATH" ]]; then
+    curl -L -z "$FILEPATH" -o "$FILEPATH" "$@"
+  else
+    curl -L -o "$FILEPATH" "$@"
+  fi
+}
+
+uncompress() {
+  local FILEPATH=$1 DEST=$2
+  
+  case "$FILEPATH" in
+    *.gz) 
+      tar xzf "$FILEPATH" -C "$DEST";;
+    *.xz) 
+      xz -dc "$FILEPATH" | tar x -C "$DEST";;
+    *.zst)
+      zstd -dc "$FILEPATH" | tar x -C "$DEST";;
+    *)
+      debug "Error: unknown package format: $FILEPATH"
+      return 1;;
+  esac
+}  
+
+###
+
+get_default_repo() {
+  local ARCH=$1
+  if [[ "$ARCH" == arm* || "$ARCH" == aarch64 ]]; then
+    echo $DEFAULT_ARM_REPO_URL
+
+  elif [[ "$ARCH" == power* || "$ARCH" == riscv64 ]]; then
+    echo $DEFAULT_POWER_REPO_URL
+  else
+    echo $DEFAULT_REPO_URL
+  fi
+}
+
+get_core_repo_url() {
+  local REPO_URL=$1 ARCH=$2
+  if [[ "$ARCH" == arm* || "$ARCH" == aarch64 ]]; then
+    echo "${REPO_URL%/}/$ARCH/core"
+
+  elif [[ "$ARCH" == power* || "$ARCH" == riscv64 ]]; then
+    echo "${REPO_URL%/}"
+  else
+    echo "${REPO_URL%/}/core/os/$ARCH"
+  fi
+}
+
+get_template_repo_url() {
+  local REPO_URL=$1 ARCH=$2
+  if [[ "$ARCH" == arm* || "$ARCH" == aarch64 ]]; then
+    echo "${REPO_URL%/}/$ARCH/\$repo"
+
+  elif [[ "$ARCH" == power* || "$ARCH" == riscv64 ]]; then
+    echo "${REPO_URL%/}"
+  else
+    echo "${REPO_URL%/}/\$repo/os/$ARCH"
+  fi
+}
+
+configure_pacman() {
+  local DEST=$1 ARCH=$2
+  debug "configure DNS and pacman"
+  cp "/etc/resolv.conf" "$DEST/etc/resolv.conf"
+  SERVER=$(get_template_repo_url "$REPO_URL" "$ARCH")
+  mkdir -p "$DEST/etc/pacman.d"
+  echo "Server = $SERVER" > "$DEST/etc/pacman.d/mirrorlist"
+}
+
+configure_minimal_system() {
+  local DEST=$1
+  
+  mkdir -p "$DEST/dev"
+  sed -ie 's/^root:.*$/root:$1$GT9AUpJe$oXANVIjIzcnmOpY07iaGi\/:14657::::::/' "$DEST/etc/shadow"
+  touch "$DEST/etc/group"
+  echo "bootstrap" > "$DEST/etc/hostname"
+
+  rm -f "$DEST/etc/mtab"
+  echo "rootfs / rootfs rw 0 0" > "$DEST/etc/mtab"
+  test -e "$DEST/dev/null" || mknod "$DEST/dev/null" c 1 3
+  test -e "$DEST/dev/random" || mknod -m 0644 "$DEST/dev/random" c 1 8
+  test -e "$DEST/dev/urandom" || mknod -m 0644 "$DEST/dev/urandom" c 1 9
+
+  sed -i "s/^[[:space:]]*\(CheckSpace\)/# \1/" "$DEST/etc/pacman.conf"
+  sed -i "s/^[[:space:]]*SigLevel[[:space:]]*=.*$/SigLevel = Never/" "$DEST/etc/pacman.conf"
+}
+
+fetch_packages_list() {
+  local REPO=$1 
+  
+  debug "fetch packages list: $REPO/"
+  fetch "$REPO/" | extract_href | awk -F"/" '{print $NF}' | sort -rn ||
+    { debug "Error: cannot fetch packages list: $REPO"; return 1; }
+}
+
+install_pacman_packages() {
+  local BASIC_PACKAGES=$1 DEST=$2 LIST=$3 DOWNLOAD_DIR=$4 ARCH=$5
+  debug "pacman package and dependencies: $BASIC_PACKAGES"
+  
+  for PACKAGE in $BASIC_PACKAGES; do
+  if [[ "$ARCH" == power* || "$ARCH" == riscv64 ]]; then
+    local FILE=$(echo "$LIST" | grep -m1 "^$PACKAGE-[[:digit:]].*\(\.gz\|\.xz\|\.zst\)$")
+    test "$FILE" || { debug "Error: cannot find package: $PACKAGE"; return 1; }
+    local FILEPATH="$DOWNLOAD_DIR/$FILE"
+    
+    if grep -m1 "\-any" <<< "$FILE" ; then
+     debug "download package: $REPO/any/$FILE"
+     fetch_file "$FILEPATH" "$REPO/any/$FILE"
+    else
+     debug "download package: $REPO/$ARCH/$FILE"
+     fetch_file "$FILEPATH" "$REPO/$ARCH/$FILE"
+    fi
+    debug "uncompress package: $FILEPATH"
+    uncompress "$FILEPATH" "$DEST"
+   else
+    local FILE=$(echo "$LIST" | grep -m1 "^$PACKAGE-[[:digit:]].*\(\.gz\|\.xz\|\.zst\)$")
+    test "$FILE" || { debug "Error: cannot find package: $PACKAGE"; return 1; }
+    local FILEPATH="$DOWNLOAD_DIR/$FILE"
+    
+    debug "download package: $REPO/$FILE"
+    fetch_file "$FILEPATH" "$REPO/$FILE"
+    debug "uncompress package: $FILEPATH"
+    uncompress "$FILEPATH" "$DEST"
+    fi
+  done
+}
+
+configure_static_qemu() {
+  local ARCH=$1 DEST=$2
+  [[ "$ARCH" == arm* ]] && ARCH=arm
+  QEMU_STATIC_BIN=$(which qemu-$ARCH-static || echo )
+  [[ -e "$QEMU_STATIC_BIN" ]] ||\
+    { debug "no static qemu for $ARCH, ignoring"; return 0; }
+  cp "$QEMU_STATIC_BIN" "$DEST/usr/bin"
+}
+
+install_packages() {
+  local ARCH=$1 DEST=$2 PACKAGES=$3
+  debug "install packages: $PACKAGES"
+   LC_ALL=C chroot "$DEST" /usr/bin/bash /usr/bin/update-ca-trust
+  LC_ALL=C chroot "$DEST" /usr/bin/pacman \
+    --noconfirm --arch $ARCH -Sy --overwrite \* $PACKAGES
+}
+
+show_usage() {
+  stderr "Usage: $(basename "$0") [-u USER] [-d DOWNLOAD_DIR] DESTDIR"
+}
+
+add_user()
+{
+	local USER=$1
+	debug "add default user"
+	chroot "$DEST" useradd -m $USER
+	chroot "$DEST" usermod -aG wheel $USER
+	printf "\n%%wheel    ALL=(ALL) ALL\n$USER    ALL=(ALL) ALL\nDefaults:$USER    \x21authenticate\n" >> "$DEST/etc/sudoers"
+}
+
+main() {
+  # Process arguments and options
+  test $# -eq 0 && set -- "-h"
+  local ARCH=
+  local REPO_URL=
+  local USE_QEMU=
+  local DOWNLOAD_DIR=
+  local PRESERVE_DOWNLOAD_DIR=
+  local USER=user
+  
+  while getopts "a:d:h:u:" ARG; do
+    case "$ARG" in
+      a) ARCH=$OPTARG;;
+      u) USER=$OPTARG;;
+      r) REPO_URL=$OPTARG;;
+      q) USE_QEMU=true;;
+      d) DOWNLOAD_DIR=$OPTARG
+         PRESERVE_DOWNLOAD_DIR=true;;
+      *) show_usage; return 1;;
+    esac
+  done
+  shift $(($OPTIND-1))
+  test $# -eq 1 || { show_usage; return 1; }
+  
+  [[ -z "$ARCH" ]] && ARCH=$(uname -m)
+  [[ -z "$REPO_URL" ]] &&REPO_URL=$(get_default_repo "$ARCH")
+  
+  local DEST=$1
+  local REPO=$(get_core_repo_url "$REPO_URL" "$ARCH")
+  [[ -z "$DOWNLOAD_DIR" ]] && DOWNLOAD_DIR=$(mktemp -d)
+  mkdir -p "$DOWNLOAD_DIR"
+  [[ -z "$PRESERVE_DOWNLOAD_DIR" ]] && trap "rm -rf '$DOWNLOAD_DIR'" KILL TERM EXIT
+  debug "destination directory: $DEST"
+  debug "core repository: $REPO"
+  debug "temporary directory: $DOWNLOAD_DIR"
+  
+  # Fetch packages, install system and do a minimal configuration
+  mkdir -p "$DEST"
+  if [[ "$ARCH" == power* || "$ARCH" == riscv64 ]]; then
+  local LIST="$(fetch_packages_list $REPO/$ARCH) $(fetch_packages_list $REPO/any)"
+  else
+  local LIST=$(fetch_packages_list $REPO)
+  fi
+  install_pacman_packages "${BASIC_PACKAGES[*]}" "$DEST" "$LIST" "$DOWNLOAD_DIR" "$ARCH"
+  configure_pacman "$DEST" "$ARCH"
+  configure_minimal_system "$DEST"
+  [[ -n "$USE_QEMU" ]] && configure_static_qemu "$ARCH" "$DEST"
+  install_packages "$ARCH" "$DEST" "${BASIC_PACKAGES[*]} ${EXTRA_PACKAGES[*]}"
+  configure_pacman "$DEST" "$ARCH" # Pacman must be re-configured
+  add_user $USER
+  [[ -z "$PRESERVE_DOWNLOAD_DIR" ]] && rm -rf "$DOWNLOAD_DIR"
+  
+  debug "Done!"
+  debug 
+  debug "You may now chroot or arch-chroot from package arch-install-scripts:"
+  debug "$ sudo arch-chroot $DEST su $USER"
+}
+
+main "$@"
